294c6ea83d23356234a3c0313269399e7a89f08e
noahallen <noahcallen@gmail.com>
2020-07-27 17:59:00 -0700

    Fixed bug where it sometimes crashed on opening

--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,8 @@ from Randomizer import *
 global randomized_board
 # randomized_board = [5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9]
 randomized_board = board_randomize()
+while(randomized_board is None):
+    randomized_board = board_randomize()
 global myButtonList 
 myButtonList= []
 global player_board
@@ -15,7 +17,6 @@ player_board= []
 #This causes random spots on our board to be replaced with "_", hiding the answers
 yes = random.randint(30,50)
 player_board = randomized_board
-randomized_board = [5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9]
 for x in range(yes):
     spot = random.randint(0,80)
     player_board[spot] = '_'
